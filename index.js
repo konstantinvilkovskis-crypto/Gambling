8e8ca0f5a6500b3ce4a5babc63463f16d18c63a0
Konstantin <konstantin.vilkovskis@gmail.com>
2026-03-28 21:48:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let itemsContainer = document.getElementById("items")
+let spinBtn = document.getElementById("spinBtn")
+let result = document.getElementById("result")
+let coins = document.getElementById("coins")
+let coinsInlet = 1000
+
+let items = [
+"🦆",
+"💎",
+"🪙",
+"🔥",
+"👑"
+]
+
+// заполняем ленту
+for(let i = 0; i < 30; i++){
+    let div = document.createElement("div")
+    div.className = "item"
+    div.textContent = items[Math.floor(Math.random() * items.length)]
+    itemsContainer.appendChild(div)
+}
+
+
+ spinBtn.addEventListener("click", function(){
+
+    if(coinsInlet >= 100){
+
+        let randomIndex = Math.floor(Math.random() * 30)
+        let offset = randomIndex * 100
+
+        itemsContainer.style.transform = "translateX(-" + offset + "px)"
+
+        spinBtn.disabled = true
+
+        setTimeout(function(){
+        spinBtn.disabled = false
+        }, 2000)
+
+        coinsInlet -= 100
+        coins.textContent = coinsInlet
+
+    } else {
+        alert('not enough currency!')
+    }
+   
+})
